c4572224751c9e5e113d7ecd3ae8d856eddf8d50
markjesusjh <143747566+markjesusjh@users.noreply.github.com>
2024-07-15 02:44:27 -0500

    Delete recomendacion.h

--- recomendacion.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECOMENDACION_H
-#define RECOMENDACION_H
-
-#include <vector>
-#include "pelicula.h"
-
-std::vector<Pelicula> recomendarPeliculas(const std::vector<Pelicula>& peliculas);
-
-#endif // RECOMENDACION_H
